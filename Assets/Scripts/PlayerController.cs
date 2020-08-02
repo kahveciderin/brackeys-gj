ddf6620826905b698aa48e63897fa45ab0bd0799
Pablo <pablogoesonline@gmail.com>
2020-08-02 13:29:05 +0200

    Doggo Added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,16 +10,16 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 10;
     public float jumpForce;
 
-    public Transform isGroundedChecker; 
-    public float checkGroundRadius; 
+    public Transform isGroundedChecker;
+    public float checkGroundRadius;
     public LayerMask groundLayer;
 
 
-    public float fallMultiplier = 2.5f; 
+    public float fallMultiplier = 2.5f;
     public float lowJumpMultiplier = 2f;
 
 
-    public float rememberGroundedFor; 
+    public float rememberGroundedFor;
     float lastTimeGrounded;
 
 
@@ -30,7 +30,7 @@ public class PlayerController : MonoBehaviour
     SpriteRenderer spriteRenderer;
 
     bool isBaby = false;
-    
+
     BoxCollider2D collider;
 
 
@@ -48,29 +48,32 @@ public class PlayerController : MonoBehaviour
     public float playSpeed = 0.1f;
 
     bool stop;
-    Stack<Vector2> playermovs = new Stack<Vector2>(); 
-    Stack<Vector2> babymovs = new Stack<Vector2>(); 
-    
+    Stack<Vector2> playermovs = new Stack<Vector2>();
+    Stack<Vector2> babymovs = new Stack<Vector2>();
 
+    private Animator animator;
 
     public int interpolaiton = 6;
-    IEnumerator reverseBaby(){
+    IEnumerator reverseBaby()
+    {
 
 
-        
+
         revEffect.SetActive(true);
 
         Vector2 previousPos = transform.position;
-        for(int i = 0; i < playermovs.Count; i++) {
-            
+        for (int i = 0; i < playermovs.Count; i++)
+        {
+
             Vector2 playbackthis = playermovs.Pop();
             Vector2 deltaPos = playbackthis - previousPos;
 
 
-            for(int j = 1; j < interpolaiton + 1; j++){
-                Vector2 addThis = j * (deltaPos / new Vector2(interpolaiton,interpolaiton)) ;
+            for (int j = 1; j < interpolaiton + 1; j++)
+            {
+                Vector2 addThis = j * (deltaPos / new Vector2(interpolaiton, interpolaiton));
                 gameObject.transform.position = previousPos + addThis;
-                
+
                 yield return new WaitForSeconds(playSpeed);
             }
             babymovs.Push(playbackthis);
@@ -83,14 +86,16 @@ public class PlayerController : MonoBehaviour
         spriteRenderer.sprite = spriteBaby;
         isBaby = true;
 
-        for(int i = 0; i < babymovs.Count; i++) {
+        for (int i = 0; i < babymovs.Count; i++)
+        {
             Vector2 playbackthis = babymovs.Pop();
             Vector2 deltaPos = playbackthis - previousPos;
 
-            for(int j = 1; j < interpolaiton + 1; j++){
-            Vector2 addThis = j * (deltaPos / new Vector2(interpolaiton,interpolaiton));
-            gameObject.transform.position =previousPos + addThis;
-            yield return new WaitForSeconds(playSpeed);
+            for (int j = 1; j < interpolaiton + 1; j++)
+            {
+                Vector2 addThis = j * (deltaPos / new Vector2(interpolaiton, interpolaiton));
+                gameObject.transform.position = previousPos + addThis;
+                yield return new WaitForSeconds(playSpeed);
             }
 
             previousPos = playbackthis;
@@ -102,22 +107,24 @@ public class PlayerController : MonoBehaviour
     }
 
 
-    public void ResetPlayer(){
+    public void ResetPlayer()
+    {
 
         lastRecorded = Time.time;
         ToAdult();
         playermovs.Clear();
         transform.position = Vector3.zero;
     }
-    void ToAdult(){
+    void ToAdult()
+    {
         collider.offset = new Vector2(0, 0);
         collider.size = new Vector2(.07f, .16f);
-        spriteRenderer.sprite = spriteAdult;
+        //spriteRenderer.sprite = spriteAdult;
         isBaby = false;
     }
     void Start()
     {
-
+        animator = GetComponent<Animator>();
 
         collider = GetComponent<BoxCollider2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
@@ -127,36 +134,42 @@ public class PlayerController : MonoBehaviour
         lastRecorded = Time.time;
     }
 
-    bool IsGrounded(){
-        Collider2D collider = Physics2D.OverlapCircle(isGroundedChecker.position, checkGroundRadius, groundLayer); 
+    bool IsGrounded()
+    {
+        Collider2D collider = Physics2D.OverlapCircle(isGroundedChecker.position, checkGroundRadius, groundLayer);
 
-        if(collider != null)
-        lastTimeGrounded = Time.time;
+        if (collider != null)
+            lastTimeGrounded = Time.time;
 
         return collider != null;
     }
 
-    void Jump() { 
-        if (Input.GetAxis("Jump") > 0) { 
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce); 
-        } 
+    void Jump()
+    {
+        if (Input.GetAxis("Jump") > 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
     }
 
 
-    void UserControl(){
+    void UserControl()
+    {
         applyMotion = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, 0);
 
-        if(IsGrounded())
-        rb.AddForce(applyMotion);
+        if (IsGrounded())
+            rb.AddForce(applyMotion);
         else
-        rb.AddForce(applyMotion/4);
-        if(IsGrounded() || (Time.time - lastTimeGrounded <= rememberGroundedFor)){
-         
-        Jump();
+            rb.AddForce(applyMotion / 4);
+        if (IsGrounded() || (Time.time - lastTimeGrounded <= rememberGroundedFor))
+        {
+
+            Jump();
         }
 
 
-        if(Input.GetAxis("Fire1") > 0){
+        if (Input.GetAxis("Fire1") > 0)
+        {
 
             stop = true;
             playermovs.Push(gameObject.transform.position);
@@ -165,24 +178,28 @@ public class PlayerController : MonoBehaviour
         }
 
 
-    if(Time.time - lastRecorded >= recordSpeed ){
-        playermovs.Push(gameObject.transform.position);
-        lastRecorded = Time.time;
-    }
+        if (Time.time - lastRecorded >= recordSpeed)
+        {
+            playermovs.Push(gameObject.transform.position);
+            lastRecorded = Time.time;
+        }
     }
 
 
-    int rndmov(){
+    int rndmov()
+    {
         float a = Random.Range(0f, 1f);
 
-            //Debug.Log(a);
+        //Debug.Log(a);
         return Mathf.Round(a) == 1 ? 1 : -1;
     }
-    void RandomControl(){
-        if(Time.time - lastdecision >= decisionMake ){
+    void RandomControl()
+    {
+        if (Time.time - lastdecision >= decisionMake)
+        {
             int mov = rndmov();
-        applyMotion = new Vector2( mov * moveSpeed, 0);
-        lastdecision = Time.time;
+            applyMotion = new Vector2(mov * moveSpeed, 0);
+            lastdecision = Time.time;
         }
 
 
@@ -190,17 +207,21 @@ public class PlayerController : MonoBehaviour
     }
     void Update()
     {
-        if(!stop){
-        if(isBaby)
-        RandomControl();
-        else
-        UserControl();
+        if (!stop)
+        {
+            if (isBaby)
+                RandomControl();
+            else
+                UserControl();
 
-    if (rb.velocity.y < 0) {
-        rb.velocity += Vector2.up * Physics2D.gravity * (fallMultiplier - 1) * Time.deltaTime;
-    } else if (rb.velocity.y > 0 && Input.GetAxis("Jump") <= 0) {
-        rb.velocity += Vector2.up * Physics2D.gravity * (lowJumpMultiplier - 1) * Time.deltaTime;
-    }   
+            if (rb.velocity.y < 0)
+            {
+                rb.velocity += Vector2.up * Physics2D.gravity * (fallMultiplier - 1) * Time.deltaTime;
+            }
+            else if (rb.velocity.y > 0 && Input.GetAxis("Jump") <= 0)
+            {
+                rb.velocity += Vector2.up * Physics2D.gravity * (lowJumpMultiplier - 1) * Time.deltaTime;
+            }
 
 
         }
