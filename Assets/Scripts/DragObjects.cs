6065fb32489977373ad4fe97501fb05a0ba4eac0
kahveciderin <kahveciderin@gmail.com>
2020-08-02 12:11:53 +0300

    Improved grid system

---
+++ Assets/Scripts/DragObjects.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragObjects : MonoBehaviour
+{
+
+    private bool isDragging;
+    private bool reachable;
+    public void OnMouseDown()
+    {
+        isDragging = true;
+    }
+
+    public void OnMouseUp()
+    {
+        isDragging = false;
+    }
+
+    void Update()
+    {
+        if (isDragging) {
+            Vector2 mousePosition = UnityEngine.Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            //reachable = true;
+            if(reachable){
+            transform.Translate(new Vector2(Mathf.Round(mousePosition.x), Mathf.Round(mousePosition.y)));
+
+            }else{
+
+     Debug.Log("Can't reach");
+            }
+        }
+    }
+
+        void OnTriggerEnter2D(Collider2D col)
+    {
+     if(col.gameObject.tag == "Reach"){
+         reachable = true;
+
+     }
+       
+     
+    }
+
+
+        void OnTriggerExit2D(Collider2D col)
+    {
+
+     if(col.gameObject.tag == "Reach"){
+     reachable = false;
+     }
+    }
+
+
+}
