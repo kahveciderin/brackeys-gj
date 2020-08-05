fdf4c59177259b38f2c2cef760441cd703a2977a
kahveciderin <kahveciderin@gmail.com>
2020-08-05 13:34:29 +0300

    Added parallax script

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+
+    public GameObject camera;
+    public GameObject[] parallaxBg;
+    public float moveDistance;
+    
+    void Update()
+    {
+        Vector2 distance = new Vector2(0 - camera.transform.position.x, 0 - camera.transform.position.y);
+
+        for(int i = 0; i < parallaxBg.Length; i++){
+            parallaxBg[i].transform.position = distance * new Vector2(i * moveDistance ,i * moveDistance);
+        }
+    }
+}
